65de85fdf3970c87544cc0367808b39a9ac3dc8a
Ubuntu <ubuntu@ip-172-31-16-210.ec2.internal>
2015-12-10 12:27:37 +0000

    minor updates including opening port 11210

--- scalability_top.py
+++ scalability_top.py
@@ -1,9 +1,4 @@
 
-# Python script to generate the cloudformation template json file
-# This is not strictly needed, but it takes the pain out of writing a
-# cloudformation template by hand.  It also allows for DRY approaches
-# to maintaining cloudformation templates.
-
 from troposphere import Ref, Template, Parameter, Output, Join, GetAtt, Tags
 import troposphere.ec2 as ec2
 import troposphere.cloudformation as cloudformation
@@ -23,17 +18,13 @@ keyname_param = t.add_parameter(Parameter(
     Description='Name of an existing EC2 KeyPair to enable SSH access'
 ))
 
-
 instance = cloudformation.Stack("vpcStack")
 instance.TemplateURL =  "https://s3.amazonaws.com/" + configuration.S3_BUCKET_NAME + "/scalability_vpc.json"
 instance.TimeoutInMinutes = 60
 t.add_resource(instance)
 
-
-
 p = {"KeyNameParameter": Ref("KeyName"), "SecurityGroupIdParameter" : GetAtt("vpcStack", "Outputs.SecurityGroupId"),
      "SubnetId1Parameter" : GetAtt("vpcStack", "Outputs.SubnetId1"), "SubnetId2Parameter" : GetAtt("vpcStack", "Outputs.SubnetId2")}
-
 
 instance = cloudformation.Stack("couchbaseStack")
 instance.TemplateURL = "https://s3.amazonaws.com/" + configuration.S3_BUCKET_NAME + "/scalability_couchbase.json"
--- scalability_vpc.py
+++ scalability_vpc.py
@@ -1,9 +1,3 @@
-
-# Python script to generate the cloudformation template json file
-# This is not strictly needed, but it takes the pain out of writing a
-# cloudformation template by hand.  It also allows for DRY approaches
-# to maintaining cloudformation templates.
-
 from troposphere import Ref, Template, Parameter, Output, Join, GetAtt, Tags
 import troposphere.ec2 as ec2
 import configuration
@@ -122,6 +116,12 @@ def createCouchbaseSecurityGroups(t, vpc):
             ToPort="8092",
             CidrIp="0.0.0.0/0",
         ),
+        ec2.SecurityGroupRule(
+            IpProtocol="tcp",
+            FromPort="11210",
+            ToPort="11210",
+            CidrIp="0.0.0.0/0",
+        ),
         ec2.SecurityGroupRule(   # sync gw user port
             IpProtocol="tcp",
             FromPort="4984",
@@ -149,7 +149,6 @@ def createCouchbaseSecurityGroups(t, vpc):
         {"FromPort": "4369", "ToPort": "4369" },    # couchbase server
         {"FromPort": "5984", "ToPort": "5984" },    # couchbase server
         {"FromPort": "11209", "ToPort": "11209" },  # couchbase server 
-        {"FromPort": "11210", "ToPort": "11210" },  # couchbase server
         {"FromPort": "11211", "ToPort": "11211" },  # couchbase server
         {"FromPort": "21100", "ToPort": "21299" },  # couchbase server
     ]
